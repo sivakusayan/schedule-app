86f04db7fd10d8dc2a09599af2ccab52db68fa61
Sayan Sivakumaran <sivakusayan@gmail.com>
2018-06-13 17:35:46 -0400

    Added overlay and made webpage responsive

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,26 +1,75 @@
+/*eslint-env browser*/
+
+/*-----------------------------------------------------------------------------*/
+/* SCHEDULE CONTROLLER */
+/*-----------------------------------------------------------------------------*/
+
 var scheduleController = (function () {
     'use strict';
     
 }());
 
+/*-----------------------------------------------------------------------------*/
+/* UI CONTROLLER */
+/*-----------------------------------------------------------------------------*/
+
 var UIController = (function () {
     'use strict';
     
+    var DOMobjects;
     
+    DOMobjects = {
+        week: document.querySelector('.nav__week'),
+        overlay: document.getElementById('overlay')
+    };
+    
+    return {
+        getDOMobjects: function () {
+            return DOMobjects;
+        },
+        
+        darkenScreen: function () {
+            DOMobjects.overlay.classList.remove('overlayOFF');
+            DOMobjects.overlayoverlay.classList.add('overlayON');
+        },
+        
+        lightenScreen: function () {
+            DOMobjects.overlay.classList.remove('overlayON');
+            DOMobjects.overlayoverlay.classList.add('overlayOFF');
+        }
+    };
     
 }());
 
+/*-----------------------------------------------------------------------------*/
+/* EVENT CONTROLLER */
+/*-----------------------------------------------------------------------------*/
+
 var eventController = (function (schedCtrl, UICtrl) {
     'use strict';
-
     
+    var setupEventListeners;
+    
+    setupEventListeners = function () {
+        var DOMobjects = UICtrl.getDOMobjects();
+        
+        DOMobjects.week.addEventListener('click', function (event) {
+    
+            if (event.target.tagName === 'BUTTON') {
+                document.querySelector('.activeDay').classList.remove('activeDay');
+                event.target.classList.add('activeDay');
+            }
+        });
+        
+        
+    };
+    
+    return {
+        init: function () {
+            setupEventListeners();
+        }
+    };
 }(scheduleController, UIController));
 
-document.querySelector('.nav__week').addEventListener('click', function (event) {
-    
-    if (event.target.tagName === 'BUTTON') {
-        document.querySelector('.activeDay').classList.remove('activeDay');
-        event.target.classList.add('activeDay');   
-    }
-});
+eventController.init();
     
