414a1249aab23105bd101b8d0aa8de17a122f16e
Sayan Sivakumaran <sivakusayan@gmail.com>
2018-07-30 10:23:14 -0400

    Reorganized architecture

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/js/index.js'],
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'js/bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      inject: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
